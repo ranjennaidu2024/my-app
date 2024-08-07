05743ef7b442ff70d6ac0ee6f831a6f5119113af
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-07 16:24:21 +0800

    Simple List

--- src/index.js
+++ src/index.js
@@ -2,110 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const firstBook = {
-  author: 'Jordan Moore',
-  title: 'Interesting Facts For Curious Minds',
-  img: './images/book-1.jpg',
-}
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-}
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book-1.jpg',
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  },
+]
 
-// function BookList() {
-//   return (
-//     <section className="booklist">
-//       <Book
-//         author={firstBook.author}
-//         title={firstBook.title}
-//         img={firstBook.img}
-//       />
-//       <Book
-//         author={secondBook.author}
-//         title={secondBook.title}
-//         img={secondBook.img}
-//       />
-//     </section>
-//   )
-// }
-
-//If let say we want to display paragraph and button for specific element , we can use children props
-//must use children and not different naming as it is react speacial prop
+const names = ['john', 'peter', 'susan']
+const newNames = names.map((name) => {
+  console.log(name)
+  return <h1>{name}</h1>
+})
 
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
-        </p>
-        <button>click me</button>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-    </section>
-  )
+  return <section className="booklist">{newNames}</section>
 }
 
-//Different approach
-//Approach 1
-// const Book = (props) => {
-//   console.log(props)
-//   return (
-//     <article className="book">
-//       <img src={props.img} alt={props.title} />
-//       <h2>{props.title}</h2>
-//       <h4>{props.author} </h4>
-//     </article>
-//   )
-// }
+const Book = (props) => {
+  const { img, title, author } = props
 
-//Approach 2
-// - no need for all the props.propName
-// - destructure inside component
-
-// const Book = (props) => {
-//   const { img, title, author } = props
-//   return (
-//     <article className="book">
-//       <img src={img} alt={title} />
-//       <h2>{title}</h2>
-//       <h4>{author} </h4>
-//     </article>
-//   )
-// }
-
-//Approach 3
-// - destructure in function parameters (in our case props)
-// - if you have console.log(props) - it won't be defined
-
-// const Book = ({ img, title, author }) => {
-//   return (
-//     <article className="book">
-//       <img src={img} alt={title} />
-//       <h2>{title}</h2>
-//       <h4>{author} </h4>
-//     </article>
-//   )
-// }
-
-//added children
-const Book = ({ img, title, author, children }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author} </h4>
-      {children}
     </article>
   )
 }
