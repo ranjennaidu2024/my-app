cf26d8cdf369196314579d948b0ed33141dee43e
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-07 15:33:31 +0800

    Props - Somewhat Dynamic Setup

--- src/index.js
+++ src/index.js
@@ -2,21 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const author = 'Jordan Moore'
-const title = 'Interesting Facts For Curious Minds'
-const img = './images/book-1.jpg'
+const firstBook = {
+  author: 'Jordan Moore',
+  title: 'Interesting Facts For Curious Minds',
+  img: './images/book-1.jpg',
+}
+const secondBook = {
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+}
 
-//if we didnt pass the author for second element it wont show
 function BookList() {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book title={title} img={img} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   )
 }
-
-//if the prop exists it will return value, otherwise no value
 const Book = (props) => {
   console.log(props)
   return (
