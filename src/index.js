4df3c6384c936dac91697bc7120a695e8bcbe3ed
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-07 13:52:56 +0800

    Nest Components

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ import ReactDOM from 'react-dom/client'
 // }
 
 //only can return one element , if multiple put in one div or use React.Fragment
-function Greeting() {
-  return (
-    <React.Fragment>
-      <div>
-        <h2>My First Component</h2>
-      </div>
-      <div>
-        <p>Testing</p>
-      </div>
-    </React.Fragment>
-  )
-}
+// function Greeting() {
+//   return (
+//     <React.Fragment>
+//       <div>
+//         <h2>My First Component</h2>
+//       </div>
+//       <div>
+//         <p>Testing</p>
+//       </div>
+//     </React.Fragment>
+//   )
+// }
 
 //using jsx easier for future , what element gonna create , empty object and what to pass
 // const Greeting = () => {
@@ -28,6 +28,21 @@ function Greeting() {
 //     React.createElement('h2', {}, 'hello world')
 //   )
 // }
+
+//Nest components
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  )
+}
+
+const Person = () => <h2>john doe</h2>
+const Message = () => {
+  return <p>this is my message</p>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
