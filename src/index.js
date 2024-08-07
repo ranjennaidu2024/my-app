9c3ba48f14ae1eb709adecc85c23105862fed45c
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-07 11:40:41 +0800

    Create element function using jsx

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 //always start with capital letter for function
-function Greeting() {
-  return <h2>My First Component</h2>
+// function Greeting() {
+//   return <h2>My First Component</h2>
+// }
+
+//using jsx easier for future , what element gonna create , empty object and what to pass
+const Greeting = () => {
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, 'hello world')
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
