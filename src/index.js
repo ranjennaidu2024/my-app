800f6d7cc262a7c530c5d18c4c83201cdffd696d
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-08 10:23:01 +0800

    Event Basics

--- src/index.js
+++ src/index.js
@@ -47,9 +47,33 @@ const books = [
 function BookList() {
   return (
     <section className="booklist">
+      <EventExamples />
       {books.map((book) => {
         return <Book {...book} key={book.id} />
       })}
+    </section>
+  )
+}
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input')
+  }
+  const handleButtonClick = () => {
+    alert('handle button click')
+  }
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
     </section>
   )
 }
