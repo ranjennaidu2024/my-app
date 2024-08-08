0ef2f734fb2deae1ef3c59d676e9182ed37b8bcc
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-08 11:18:35 +0800

    Prop Drilling

--- src/index.js
+++ src/index.js
@@ -18,26 +18,27 @@ const books = [
 ]
 
 function BookList() {
+  const someValue = 'shakeAndBake'
+  const displayValue = () => {
+    console.log(someValue)
+  }
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} />
+        return <Book {...book} key={book.id} displayValue={displayValue} />
       })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { img, title, author } = props
-  const displayTitle = () => {
-    console.log(title)
-  }
+  const { img, title, author, displayValue } = props
 
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayTitle}>display title</button>
+      <button onClick={displayValue}>click me</button>
       <h4>{author} </h4>
     </article>
   )
