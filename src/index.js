d05b9926b80cac111fb092635a7b823ad8b3913c
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-08 15:32:45 +0800

    Numbers Challenge

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 //just to show you can use two approach , just add expor before const above , where need to follow same name
-//if use export default then when import can put whatever name you wanted by specifying the path , for this we use Banana for Book class
+//if use export default then when import can put whatever name you wanted by specifying the path , for this we can use Banana for Book class
 import { books } from './books'
-import Banana from './Book'
+import Book from './Book'
 
-function BookList() {
+const BookList = () => {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Banana {...book} key={book.id} />
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />
       })}
     </section>
   )
